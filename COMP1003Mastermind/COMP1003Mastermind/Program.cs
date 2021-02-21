32b667c6d839bb13d4b392e6f14b600b461960d3
Louis Devlin <72494457+Louis-Devlin@users.noreply.github.com>
2021-02-21 14:49:14 +0000

    Initial Commit

---
+++ COMP1003Mastermind/COMP1003Mastermind/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace COMP1003Mastermind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
